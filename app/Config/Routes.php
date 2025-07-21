c9181f90f658396e554227e3beaa6b4fd04ea726
Nurfitriarp <93233737+Nurfitriarp@users.noreply.github.com>
2025-07-21 18:11:53 +0700

    ganti route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('home', 'Home::index');
 $routes->get('how', 'How::index');
 $routes->get('team', 'Team::index');
 $routes->get('social', 'Social::index');
